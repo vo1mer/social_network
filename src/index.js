dc0ae68f616bcfcfeac5b4c2f185c29910ccfefb
Vo1mer <volodymyr.khomovyi@gmail.com>
2019-09-07 23:15:23 +0300

    debug reducers

--- src/index.js
+++ src/index.js
@@ -7,19 +7,10 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
-let rerenderEntireTree = (state) => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App state={state}/>
-            </Provider>
-        </BrowserRouter>, document.getElementById('root'));
-};
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>, document.getElementById('root'));
 
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
-});
